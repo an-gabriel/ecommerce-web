cfd592a685ada299269493031d8358ff3f45e6f6
Gabriel Aquino <gabriel.aquino@contraclabs.tech>
2024-06-15 22:08:41 -0300

    build: updagrade

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,43 @@
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
   },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "webpack.config.js"),
+          to: path.resolve(__dirname, "build"),
+        },
+      ],
+    }),
+  ],
   devServer: {
     hot: false,
     liveReload: false,
